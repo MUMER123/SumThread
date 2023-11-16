5dcb767229cc5615b7d8bb450fdab1f07f961c2c
MUMER123 <uumer2428@gmail.com>
2023-11-16 15:36:23 +0500

    Sum of List using Thread

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many number to input: ");
+        int range = sc.nextInt();
+        int[] arr = new int[range];
+
+        for(int i=0;i<range;i++){
+           arr[i] = input();
+        }
+        MyThread t = new MyThread("sum",arr);
+        t.start();
+
+
+    }
+    public static int input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Positive Integer: ");
+        int num = sc.nextInt();
+        if(num<0){
+            System.out.println("Only Positive numbers allowed...");
+            return input();
+        }
+        else{
+            return num;
+        }
     }
 }
--- src/MyThread.java
+++ src/MyThread.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MyThread {
+public class MyThread extends Thread{
+    private int[] arr;
+
+    MyThread(String name, int[] arr){
+        super(name);
+        this.arr = arr;
+    }
+    @Override
+    public void run(){
+        int sum = 0;
+        for(int i:arr){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
 }
